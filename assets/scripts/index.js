ac20cb291b4d9d2b31c4e4932a2f15dbe8388d96
LukeKeltner <lkeltner87@gmail.com>
2017-11-14 09:54:16 -0500

    working away

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,9 +1,18 @@
-$(document).on("click", ".project-image", function(event)
+$(".project-image").on("click", function(event)
 {
+	var id = $(this).attr("id");
 	$("#project-list").fadeOut(500)
+	window.location="/#projects"
 	setTimeout(function()
 	{
 		$("#specific-project").fadeIn()
+		console.log(id)
+
+		if (id==="riddle")
+		{
+			$('#project-image-insert').html('<img src="images/riddle.png" style="height:600px;"></img>')
+			$('#project-description').html("This is a Riddle Game.  Features include...")
+		}
 	},400)
 })
 
