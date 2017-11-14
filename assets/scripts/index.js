1cd1fa577b307149e9bf71cb08c3bba198ff3a73
LukeKeltner <lkeltner87@gmail.com>
2017-11-14 15:59:47 -0500

    adding colors

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,8 +10,12 @@ $(".project-image").on("click", function(event)
 
 		if (id==="riddle")
 		{
-			$('#project-image-insert').html('<img src="images/riddle.png" style="height:600px;"></img>')
-			$('#project-description').html("This is a Riddle Game.  Features include...")
+			$('#project-image-insert').html('<h1><i class="fa fa-question fa-5x project-image" aria-hidden="true" id="riddle"></i></h1>')
+			$('#project-description').html("This Riddle Game rewards players with game currency and level ups for successfully answering riddles correctly.")
+			$('#project-features').html("<li>Individual user experience</li><li>SQL Database with secured user table, riddle table, and lookup table</li><li>Users never see a riddle they have gotten correct twice</li><li>Leaderboard that highlights the user and their rank among all other users. Rank based on amount of coins and level</li>")
+			$('#project-tech').html("<li>Node and Express</li><li>Express Handlebars</li><li>SQL Database with custom made ORM</li><li>AES-js</li><li>Validator npm</li><li>mysql npm</li>")
+			$('#heroku-link').html('<a href="http://riddlegame.herokuapp.com/" target="_blank"><i class="fa fa-gamepad fa-5x" aria-hidden="true"></i></a><h2>Try it out!</h2>')
+			$('#github-link').html('<a href="https://github.com/riddle-dudes/riddle-project" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a>')
 		}
 	},400)
 })
