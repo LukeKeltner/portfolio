89554c28d3f744a861b8de0699adc77ffbc13a29
Luke Keltner <lkeltner87@gmail.com>
2017-11-14 21:18:08 -0500

    going to bed

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -32,10 +32,30 @@ $(".project-image").on("click", function(event)
 		{
 			$('#project-image-insert').html('<h1><i class="fa fa-globe fa-5x project-image" aria-hidden="true" id="riddle"></i></h1>')
 			$('#project-description').html("API Fun was developed to demonstrate the ease, scope, and entertainment of APIs")
-			$('#project-features').html("<li>Gif search function that produces a given amount of gifs searched by a user input</li><li>Movie database search that plots movies' IMDB scores against each other. User chooses all movies and can reset graph at any time.</li><li>Map and Weather function where user clicks anywhere on the globe and receives the following information on where they clicked:<ul><li>Latitude</li><li>Longitude</li><li>Area</li><li>Weather</li><li>Temperature</li><li>Humidity</li><li>Wind Speed</li></ul></li>")
+			$('#project-features').html("<li>Gif search function that produces a given amount of gifs searched by a user input, includes search history</li><li>Movie database search that plots movies' IMDB scores against each other. User chooses all movies and can reset graph at any time.</li><li>Map and Weather function where user clicks anywhere on the globe and receives the following information on where they clicked:<ul><li>Latitude</li><li>Longitude</li><li>Area</li><li>Weather</li><li>Temperature</li><li>Humidity</li><li>Wind Speed</li></ul></li>")
 			$('#project-tech').html("<li>Bootstrap for mobile friendly use</li><li>JQUERY ajax</li><li>c3.js</li><li>APIs include:<ul><li>GIPHY</li><li>OMDB</li><li>Mapbox</li><li>OpenWeatherMap</li></ul></li>")
 			$('#heroku-link').html('<a href="/apifun" target="_blank"><i class="fa fa-eye fa-5x" aria-hidden="true"></i></a><h2>Check it out!</h2>')
 			$('#github-link').html('<a href="https://github.com/LukeKeltner/API-Fun" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a>')
+		}
+
+		if (id === "rf")
+		{
+			$('#project-image-insert').html('<h1><i class="fa fa-heartbeat fa-5x project-image" aria-hidden="true" id="riddle"></i></h1>')
+			$('#project-description').html("Spinal Surgery Complication Predictor - developed for The Cleveland Medical Hackathon")
+			$('#project-features').html("<li>Web app that lets doctors input patient information and predicts probability of patient having complications after a spinal surgery</li><li>Doctors can use from over 300 diagnoses and past medical complications from searchable tables to improve predictability<li>Implements a custom written Repeated Random Balanced Sub-Sampling Random Forest algorithm for appreciable accuracy</li>")
+			$('#project-tech').html("<li>Node and Express</li><li>Child Process</li><li>Python<ul><li>pandas for data cleaning and processing</li><li>scikit-learn for Random Forest algorithm</li><li>Created Repeated Random Balanced Sub Sampling algorithm to account for low positive complication counts to increase accuracy.</li></ul></li>")
+			$('#heroku-link').html('<a href="https://www.youtube.com/watch?v=FWMhkfqMGP8" target="_blank"><i class="fa fa-youtube-play fa-5x" aria-hidden="true"></i></a><h2>What the Demo!</h2>')
+			$('#github-link').html('<a href="https://github.com/Medical-Hackathon/project" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a>')
+		}
+
+		if (id === "hangman")
+		{
+			$('#project-image-insert').html('<h1><i class="fa fa-font fa-5x project-image" aria-hidden="true" id="riddle"></i></h1>')
+			$('#project-description').html("A fun Hangman game to demonstrate the ease and power of Javascript")
+			$('#project-features').html("<li>Web app that lets doctors input patient information and predicts probability of patient having complications after a spinal surgery</li><li>Doctors can use from over 300 diagnoses and past medical complications from searchable tables to improve predictability<li>Implements a custom written Repeated Random Balanced Sub-Sampling Random Forest algorithm for appreciable accuracy</li>")
+			$('#project-tech').html("<li>Node and Express</li><li>Child Process</li><li>Python<ul><li>pandas for data cleaning and processing</li><li>scikit-learn for Random Forest algorithm</li><li>Created Repeated Random Balanced Sub Sampling algorithm to account for low positive complication counts to increase accuracy.</li></ul></li>")
+			$('#heroku-link').html('<a href="https://www.youtube.com/watch?v=FWMhkfqMGP8" target="_blank"><i class="fa fa-youtube-play fa-5x" aria-hidden="true"></i></a><h2>What the Demo!</h2>')
+			$('#github-link').html('<a href="https://github.com/Medical-Hackathon/project" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a>')
 		}
 	},400)
 })
--- controllers/contoller.js
+++ controllers/contoller.js
@@ -11,4 +11,19 @@ router.get("/apifun", function(req, res)
 	res.render("api-index");
 })
 
+router.get("/giphy", function(req, res)
+{
+	res.render("giphy");
+})
+
+router.get("/omdb", function(req, res)
+{
+	res.render("omdb");
+})
+
+router.get("/mapbox", function(req, res)
+{
+	res.render("mapbox");
+})
+
 module.exports = router;
