2ad6cb77bcf8489d887ca1612f31cec618a45109
Luke Keltner <lkeltner87@gmail.com>
2017-11-15 06:19:23 -0500

    finished projects

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,8 @@
 $(".project-image").on("click", function(event)
 {
+	window.location="/#projects"
 	var id = $(this).attr("id");
 	$("#project-list").fadeOut(500)
-	window.location="/#projects"
 	setTimeout(function()
 	{
 		$("#specific-project").fadeIn()
@@ -14,7 +14,7 @@ $(".project-image").on("click", function(event)
 			$('#project-description').html("This Riddle Game rewards players with game currency and level ups for successfully answering riddles correctly")
 			$('#project-features').html("<li>Individual user experience</li><li>SQL Database with secured user table, riddle table, and lookup table</li><li>Users never see a riddle they have gotten correct twice</li><li>Leaderboard that highlights the user and their rank among all other users. Rank based on amount of coins and level</li>")
 			$('#project-tech').html("<li>Node and Express</li><li>Express Handlebars</li><li>SQL Database with custom made ORM</li><li>AES-js</li><li>validator npm</li><li>mysql npm</li><li>Bootstrap</li>")
-			$('#heroku-link').html('<a href="http://riddlegame.herokuapp.com/" target="_blank"><i class="fa fa-gamepad fa-5x" aria-hidden="true"></i></a><h2>Try it out!</h2>')
+			$('#heroku-link').html('<a href="http://riddlegame.herokuapp.com/" target="_blank"><i class="fa fa-gamepad fa-5x" aria-hidden="true"></i></a><h2>Play me!</h2>')
 			$('#github-link').html('<a href="https://github.com/riddle-dudes/riddle-project" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a>')
 		}
 
@@ -44,18 +44,18 @@ $(".project-image").on("click", function(event)
 			$('#project-description').html("Spinal Surgery Complication Predictor - developed for The Cleveland Medical Hackathon")
 			$('#project-features').html("<li>Web app that lets doctors input patient information and predicts probability of patient having complications after a spinal surgery</li><li>Doctors can use from over 300 diagnoses and past medical complications from searchable tables to improve predictability<li>Implements a custom written Repeated Random Balanced Sub-Sampling Random Forest algorithm for appreciable accuracy</li>")
 			$('#project-tech').html("<li>Node and Express</li><li>Child Process</li><li>Python<ul><li>pandas for data cleaning and processing</li><li>scikit-learn for Random Forest algorithm</li><li>Created Repeated Random Balanced Sub Sampling algorithm to account for low positive complication counts to increase accuracy.</li></ul></li>")
-			$('#heroku-link').html('<a href="https://www.youtube.com/watch?v=FWMhkfqMGP8" target="_blank"><i class="fa fa-youtube-play fa-5x" aria-hidden="true"></i></a><h2>What the Demo!</h2>')
+			$('#heroku-link').html('<a href="https://www.youtube.com/watch?v=FWMhkfqMGP8" target="_blank"><i class="fa fa-youtube-play fa-5x" aria-hidden="true"></i></a><h2>Watch the Demo!</h2>')
 			$('#github-link').html('<a href="https://github.com/Medical-Hackathon/project" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a>')
 		}
 
-		if (id === "hangman")
+		if (id === "bamazon")
 		{
-			$('#project-image-insert').html('<h1><i class="fa fa-font fa-5x project-image" aria-hidden="true" id="riddle"></i></h1>')
-			$('#project-description').html("A fun Hangman game to demonstrate the ease and power of Javascript")
-			$('#project-features').html("<li>Web app that lets doctors input patient information and predicts probability of patient having complications after a spinal surgery</li><li>Doctors can use from over 300 diagnoses and past medical complications from searchable tables to improve predictability<li>Implements a custom written Repeated Random Balanced Sub-Sampling Random Forest algorithm for appreciable accuracy</li>")
-			$('#project-tech').html("<li>Node and Express</li><li>Child Process</li><li>Python<ul><li>pandas for data cleaning and processing</li><li>scikit-learn for Random Forest algorithm</li><li>Created Repeated Random Balanced Sub Sampling algorithm to account for low positive complication counts to increase accuracy.</li></ul></li>")
-			$('#heroku-link').html('<a href="https://www.youtube.com/watch?v=FWMhkfqMGP8" target="_blank"><i class="fa fa-youtube-play fa-5x" aria-hidden="true"></i></a><h2>What the Demo!</h2>')
-			$('#github-link').html('<a href="https://github.com/Medical-Hackathon/project" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a>')
+			$('#project-image-insert').html('<h1><i class="fa fa-shopping-bag fa-5x project-image" aria-hidden="true" id="riddle"></i></h1>')
+			$('#project-description').html("Bamazon simulates an online store complete with customer, manager, and supervisor funtionality")
+			$('#project-features').html("<li>CLI-table package allows for clean tables for client viewing</li><li>Interactive cli interface allows:<ul><li>Customers to buy items</li><li>Managers to view items with their departments and stock quantities and items in low stock, restock items, and add new items to current departments.</li><li>Supervisors to view the Product by Sales table showing them how much money each department has made, the over head costs of each department, and the total profits which is the subtraction of over head costs from product sales. They also are able to create new departments.</ul></li>")
+			$('#project-tech').html("<li>Node</li><li>Inquirer</li><li>cli-table</li><li>mysql - Products and Departments tables with relational queries</li>")
+			$('#heroku-link').html('<a href="https://www.youtube.com/watch?v=9YW2XWnSX3c&t=2s" target="_blank"><i class="fa fa-youtube-play fa-5x" aria-hidden="true"></i></a><h2>Watch the Demo!</h2>')
+			$('#github-link').html('<a href="https://github.com/LukeKeltner/bamazon" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a><h2>Clone and play!</h2>')
 		}
 	},400)
 })
