ccd7d9f6010c03d55f4c2c032509805b483a9897
Luke Keltner <lkeltner87@gmail.com>
2017-11-16 06:20:50 -0500

    almost done!

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -15,7 +15,7 @@ $(".project-image").on("click", function(event)
 			$('#project-features').html("<li>Individual user experience</li><li>SQL Database with secured user table, riddle table, and lookup table</li><li>Users never see a riddle they have gotten correct twice</li><li>Leaderboard that highlights the user and their rank among all other users. Rank based on amount of coins and level</li>")
 			$('#project-tech').html("<li>Node and Express</li><li>Express Handlebars</li><li>SQL Database with custom made ORM</li><li>AES-js</li><li>validator npm</li><li>mysql npm</li><li>Bootstrap</li>")
 			$('#heroku-link').html('<a href="http://riddlegame.herokuapp.com/" target="_blank"><i class="fa fa-gamepad fa-5x" aria-hidden="true"></i></a><h2>Play me!</h2>')
-			$('#github-link').html('<a href="https://github.com/riddle-dudes/riddle-project" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a>')
+			$('#github-link').html('<a href="https://github.com/riddle-dudes/riddle-project" target="_blank"><i class="fa fa-github-square shadow fa-5x" aria-hidden="true"></i></a>')
 		}
 
 		if (id === "tom")
@@ -25,7 +25,7 @@ $(".project-image").on("click", function(event)
 			$('#project-features').html("<li>Developed to have the classical look</li><li>A song table with over 80 entries searchable by title, artist, lyricist, genre, or year</li><li>Photo Gallery constructed using Bootstrap</li><li>Song List and Photo Gallery constructed from JSON objects easy maintance and updating.</li><li>Constact Me section constructed with nodemailer</li>")
 			$('#project-tech').html("<li>Node and Express</li><li>nodemailer</li><li>Bootstrap for mobile friendly use</li><li>fs-extra</li>")
 			$('#heroku-link').html('<a href="http://tomkeltner.herokuapp.com/" target="_blank"><i class="fa fa-eye fa-5x" aria-hidden="true"></i></a><h2>Check it out!</h2>')
-			$('#github-link').html('<a href="https://github.com/LukeKeltner/tomkeltner-repo" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a>')
+			$('#github-link').html('<a href="https://github.com/LukeKeltner/tomkeltner-repo" target="_blank"><i class="fa fa-github-square shadow fa-5x" aria-hidden="true"></i></a>')
 		}
 
 		if (id === "apifun")
@@ -35,7 +35,7 @@ $(".project-image").on("click", function(event)
 			$('#project-features').html("<li>Gif search function that produces a given amount of gifs searched by a user input, includes search history</li><li>Movie database search that plots movies' IMDB scores against each other. User chooses all movies and can reset graph at any time.</li><li>Map and Weather function where user clicks anywhere on the globe and receives the following information on where they clicked:<ul><li>Latitude and Longitude</li><li>Area</li><li>Weather</li><li>Temperature, Humidity, and Wind Speed</li></ul></li>")
 			$('#project-tech').html("<li>Bootstrap for mobile friendly use</li><li>JQUERY ajax</li><li>c3.js</li><li>APIs include:<ul><li>GIPHY</li><li>OMDB</li><li>Mapbox</li><li>OpenWeatherMap</li></ul></li>")
 			$('#heroku-link').html('<a href="/apifun" target="_blank"><i class="fa fa-eye fa-5x" aria-hidden="true"></i></a><h2>Check it out!</h2>')
-			$('#github-link').html('<a href="https://github.com/LukeKeltner/API-Fun" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a>')
+			$('#github-link').html('<a href="https://github.com/LukeKeltner/API-Fun" target="_blank"><i class="fa fa-github-square shadow fa-5x" aria-hidden="true"></i></a>')
 		}
 
 		if (id === "rf")
@@ -45,7 +45,7 @@ $(".project-image").on("click", function(event)
 			$('#project-features').html("<li>Web app that lets doctors input patient information and predicts probability of patient having complications after a spinal surgery</li><li>Doctors can use from over 300 diagnoses and past medical complications from searchable tables to improve predictability<li>Implements a custom written Repeated Random Balanced Sub-Sampling Random Forest algorithm for appreciable accuracy</li>")
 			$('#project-tech').html("<li>Node and Express</li><li>Child Process</li><li>Python<ul><li>pandas for data cleaning and processing</li><li>scikit-learn for Random Forest algorithm</li><li>Created Repeated Random Balanced Sub Sampling algorithm to account for low positive complication counts to increase accuracy.</li></ul></li>")
 			$('#heroku-link').html('<a href="https://www.youtube.com/watch?v=FWMhkfqMGP8" target="_blank"><i class="fa fa-youtube-play fa-5x" aria-hidden="true"></i></a><h2>Watch the Demo!</h2>')
-			$('#github-link').html('<a href="https://github.com/Medical-Hackathon/project" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a>')
+			$('#github-link').html('<a href="https://github.com/Medical-Hackathon/project" target="_blank"><i class="fa fa-github-square shadow fa-5x" aria-hidden="true"></i></a>')
 		}
 
 		if (id === "bamazon")
@@ -55,7 +55,7 @@ $(".project-image").on("click", function(event)
 			$('#project-features').html("<li>CLI-table package allows for clean tables for client viewing</li><li>Interactive cli interface allows:<ul><li>Customers to buy items</li><li>Managers to view items with their departments and stock quantities and items in low stock, restock items, and add new items to current departments.</li><li>Supervisors to view the Product by Sales table showing them how much money each department has made, the over head costs of each department, and the total profits which is the subtraction of over head costs from product sales. They also are able to create new departments.</ul></li>")
 			$('#project-tech').html("<li>Node</li><li>Inquirer</li><li>cli-table</li><li>mysql - Products and Departments tables with relational queries</li>")
 			$('#heroku-link').html('<a href="https://www.youtube.com/watch?v=9YW2XWnSX3c&t=2s" target="_blank"><i class="fa fa-youtube-play fa-5x" aria-hidden="true"></i></a><h2>Watch the Demo!</h2>')
-			$('#github-link').html('<a href="https://github.com/LukeKeltner/bamazon" target="_blank"><i class="fa fa-github-square fa-5x" aria-hidden="true"></i></a><h2>Clone and play!</h2>')
+			$('#github-link').html('<a href="https://github.com/LukeKeltner/bamazon" target="_blank"><i class="fa fa-github-square shadow fa-5x" aria-hidden="true"></i></a><h2>Clone and play!</h2>')
 		}
 	},100)
 })
